2d0bb1c7a343aec600e69e926c599a7f0f1d1c95
blitzstrahl111 <166958230+blitzstrahl111@users.noreply.github.com>
2024-04-22 23:01:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var password = process.env.password;
 var shared_secret = process.env.shared;
 
 var games = [730, 440, 570, 578080];  // Enter here AppIDs of the needed games
-var status = 1;  // 1 - online, 7 - invisible
+var status = 7;  // 1 - online, 7 - invisible
 
 var user = new steamUser();
 user.logOn({"accountName": username, "password": password, "twoFactorCode": steamTotp.generateAuthCode(shared_secret)});
@@ -22,7 +22,7 @@ var password2 = process.env.password2;
 var shared_secret2 = process.env.shared2;
 
 var games2 = [730, 440, 570, 578080];  // Enter here AppIDs of the needed games
-var status2 = 1;  // 1 - online, 7 - invisible
+var status2 = 7;  // 1 - online, 7 - invisible
 
 var user2 = new steamUser();
 user2.logOn({"accountName": username2, "password": password2, "twoFactorCode": steamTotp.generateAuthCode(shared_secret2)});
